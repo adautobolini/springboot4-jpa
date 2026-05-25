4ce08ff2ac9267d8b9020c86650b5d82c1701d60
JoDauto <josebolinijr@gmail.com>
2026-05-25 19:37:41 -0300

    Service layer, component registration

---
+++ src/main/java/com/estudandojava/coursejava/services/UserService.java
@@ -0,0 +1,29 @@
+package com.estudandojava.coursejava.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudandojava.coursejava.entities.User;
+import com.estudandojava.coursejava.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository; 
+	
+/* Nao coloca anotation @Repository no UserRepository, pois tal interface implementa o
+ *  JpaRepository que ja esta registrado como Componente para Injecao de Independencia */
+
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findbyId(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
